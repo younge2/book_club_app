4f192ed02db7aa37158f2af2e78e05d12ba6d818
michaelscotthurley <michaelscotthurley>
2016-03-11 15:24:33 -0800

    Adding local auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   root 'main#index'
 
+  #Facebook authenication routes
   get 'auth/:provider/callback' => 'auth#callback'
   get 'auth/logout' => 'auth#logout'
+  
+  #Local authentication routes
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
