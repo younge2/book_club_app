e80867af7af7d215f11d2c5ad6f61c2dde311c2b
Nepios <nepios2@gmail.com>
2016-03-12 15:36:27 -0800

    display thumbnails

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback' => 'auth#callback'
   get 'auth/logout' => 'auth#logout'
-  get 'books/show'  => 'books#show'
+  get 'books'  => 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
